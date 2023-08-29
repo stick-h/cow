de3bccf0eaa5936ad128ad16f385e1ead011d2a8
stick-h <68358227+stick-h@users.noreply.github.com>
2023-08-28 23:13:11 -0400

    math.2

--- src/cow/plug/CowEvents.java
+++ src/cow/plug/CowEvents.java
@@ -22,56 +22,75 @@ public class CowEvents implements Listener {
 	
 	@EventHandler
 	public static void onEntityBreed(EntityBreedEvent evt) throws IOException {
-		System.out.println("bred");
-		Ageable child = (Ageable) evt.getEntity();
-		String childID = child.getUniqueId().toString();
+		Ageable childEntity = (Ageable) evt.getEntity();
+		String childID = childEntity.getUniqueId().toString();
 		String fatherID = evt.getFather().getUniqueId().toString();
 		String motherID = evt.getMother().getUniqueId().toString();
 		Location location = evt.getFather().getLocation();
 		World world = evt.getFather().getWorld();
 		
-		double COI = 0.00;
+		JSONObject father = (JSONObject)nodes.get(fatherID);
+		JSONObject mother = (JSONObject)nodes.get(motherID);
 		JSONArray fatherTree = null;
 		JSONArray motherTree = null;
+		
+		double COI = 0.00;
 		List<Integer> nums = new ArrayList<Integer>();
 		List<Double> COIs = new ArrayList<Double>();
 		
-		JSONObject fatherObj = (JSONObject)nodes.get(fatherID);
-		JSONObject motherObj = (JSONObject)nodes.get(motherID);
-		
-		
-		if(fatherObj == null){
-			JSONArray first = new JSONArray();
-			first.add(fatherID);
+		//[[gen0], [gen1], [gen2],...]
+		//[[{"id": parentID, child: childID}], [{}, {}],...]
+		if(father == null){
+			JSONObject fatherObj = new JSONObject();
+			JSONArray array = new JSONArray();
+			
+			fatherObj.put("id", fatherID);
+			fatherObj.put("child", null);
+			
+			array.add(fatherObj);
 			fatherTree = new JSONArray();
-			fatherTree.add(first);
+			fatherTree.add(array);
 			
-			fatherObj = new JSONObject();
-			fatherObj.put("tree", fatherTree);
-			fatherObj.put("COI", 0.00);
-			nodes.put(fatherID, fatherObj);
+			father = new JSONObject();
+			father.put("tree", fatherTree);
+			father.put("COI", 0.00);
+			nodes.put(fatherID, father);
 			
 			FileWriter filewriter = new FileWriter(file);
 			filewriter.write(nodes.toJSONString());
 			filewriter.close();
-		}else fatherTree = (JSONArray)fatherObj.get("tree");
+		}else fatherTree = (JSONArray)father.get("tree");
 		
-		if(motherObj == null){
-			JSONArray first = new JSONArray();
-			first.add(motherID);
-			motherTree = new JSONArray();
-			motherTree.add(first);
+		if(mother == null){
+			JSONObject motherObj = new JSONObject();
+			motherObj.put("id", motherID);
+			motherObj.put("child", null);
 			
-			motherObj = new JSONObject();
-			motherObj.put("tree", motherTree);
-			motherObj.put("COI", 0.00);
-			nodes.put(motherID, motherObj);
+			JSONArray array = new JSONArray();
+			array.add(motherObj);
+			motherTree = new JSONArray();
+			motherTree.add(array);
+			
+			mother = new JSONObject();
+			mother.put("tree", motherTree);
+			mother.put("COI", 0.00);
+			nodes.put(motherID, mother);
 			
 			FileWriter filewriter = new FileWriter(file);
 			filewriter.write(nodes.toJSONString());
 			filewriter.close();
-		}else motherTree = (JSONArray)motherObj.get("tree");
-
+		}else motherTree = (JSONArray)mother.get("tree");
+		
+		/*
+		 * finding possible matches between fatherTree and motherTree
+		 * if same entity found with different child (i.e. different initial and return paths), indicates loop
+		 * multiple loops can exist per entity
+		 * 
+		 * COI = sum of COI contribution of each loop
+		 * COI contribution = (0.5^(n-1))(1+f)
+		 * n = number of members in loop (determined by index in father and mother trees)
+		 * f = COI of ancestor
+		 */
 		for(int i = 0; i < fatherTree.size(); i++){
 			
 			for(int j = 0; j < motherTree.size(); j++){
@@ -80,60 +99,80 @@ public class CowEvents implements Listener {
 				
 				for(int k = 0; k < genF.size(); k++){
 					
-					String entity = (String)genF.get(k);
+					JSONObject entityObjF = (JSONObject)genF.get(k);
 					int count = 0;
 					
-					for(int m = 0; m < genM.size(); m++) if(entity == (String)genM.get(m)) count++;
+					for(int m = 0; m < genM.size(); m++){
+						JSONObject entityObjM = (JSONObject)genM.get(m);
+						if(entityObjF.get("id").equals(entityObjM.get("id")) && !entityObjF.get("child").equals(entityObjM.get("child"))) count++;
+					}
 					
 					for(int n = 0; n < count; n++){
 						nums.add(i+j+1);
-						JSONObject entityObj = (JSONObject)nodes.get(entity);
-						if(entityObj != null) COIs.add((Double)entityObj.get("COI"));
+						JSONObject entity = (JSONObject)nodes.get(entityObjF.get("id"));
+						if(entity != null) COIs.add((Double)entity.get("COI"));
 						else COIs.add(0.00);
 					}
 					
 				}
 			}
 		}
-		
-		for(int i = 0; i < nums.size(); i++) COI += Math.pow(0.5, nums.get(i))*(1+COIs.get(i));
 
+		for(int i = 0; i < nums.size(); i++) COI += Math.pow(0.5, nums.get(i))*(1+COIs.get(i));
 		double random = Math.random();
-		System.out.println(random);
+		
 		if(random < COI){
-			child.remove();
-			switch(child.getType()){
+			childEntity.remove();
+			switch(childEntity.getType()){
 				case PIG:
 					Creeper creeper = (Creeper)world.spawnEntity(location, EntityType.CREEPER);
 					creeper.ignite();
 					break;
 				default:
 					LivingEntity ravager = (LivingEntity)world.spawnEntity(location, EntityType.RAVAGER);
-					ravager.setCustomName(child.getType().toString());
+					ravager.setCustomName(childEntity.getType().toString());
 					break;
 			}
 		}else{
+			JSONObject child = new JSONObject();
 			JSONArray childTree = new JSONArray();
-			JSONArray first = new JSONArray();
-			first.add(childID);
-			childTree.add(first);
 			
-			for(int i = 0; i < Math.max(fatherTree.size(), motherTree.size()); i++){
+			JSONObject childObj = new JSONObject();
+			JSONArray array0 = new JSONArray();
+			
+			JSONObject fatherObj = new JSONObject();
+			JSONObject motherObj = new JSONObject();
+			JSONArray array1 = new JSONArray();
+			
+			childObj.put("id", childID);
+			childObj.put("child", null);
+			
+			array0.add(childObj);
+			childTree.add(array0);
+			
+			fatherObj.put("id", fatherID);
+			fatherObj.put("child", childID);
+			motherObj.put("id", motherID);
+			motherObj.put("child", childID);
+			
+			array1.add(fatherObj);
+			array1.add(motherObj);
+			childTree.add(array1);
+			
+			for(int i = 1; i < Math.max(fatherTree.size(), motherTree.size()); i++){
 				JSONArray jointArray = new JSONArray();
 				if(fatherTree.size() > i) jointArray.addAll((JSONArray)fatherTree.get(i));
 				if(motherTree.size() > i) jointArray.addAll((JSONArray)motherTree.get(i));
 				childTree.add(jointArray);
 			}
 			
-			JSONObject childObj = new JSONObject();
-			childObj.put("tree", childTree);
-			childObj.put("COI", COI);
-			nodes.put(childID, childObj);
+			child.put("tree", childTree);
+			child.put("COI", COI);
+			nodes.put(childID, child);
 			
 			FileWriter filewriter = new FileWriter(file);
 			filewriter.write(nodes.toJSONString());
 			filewriter.close();
 		}
-		System.out.println(COI);
 	}
 }
